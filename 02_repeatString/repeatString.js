2ad25cce6d85a4c9ad8aa2de2690c5df50a9d6c6
Stefan <stefan.ro95@gmail.com>
2022-07-31 19:30:07 +0300

    resolve exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    if (num<0){
+        return 'ERROR';
+    }
 
+    let hold = '';
+
+    for (i = 0; i < num; i++) {
+        hold += str;
+    }
+
+    return hold;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+    if ((firstNum < 0 || secondNum < 0) || (typeof (firstNum) != 'number' || typeof (secondNum) != 'number')) {
+        return 'ERROR';
+    }
 
+    if (firstNum > secondNum) {
+        [firstNum, secondNum] = [secondNum, firstNum];
+    }
+
+    let sum = 0;
+    for (i = firstNum; i <= secondNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
