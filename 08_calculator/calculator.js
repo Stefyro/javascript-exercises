42c456a069a3bf4685e5d4641c82f25c08687e9a
Stefan <stefan.ro95@gmail.com>
2022-08-16 22:37:54 +0300

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+
+  for (part of array) {
+    sum += part;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  let prod = 1;
 
+  for (part of array) {
+    prod *= part;
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let factorial = 1;
+  if (num > 0) {
+    for (i = 1; i <= num; i++) {
+      factorial *= i;
+    }
+  }
+  return factorial;
 };
 
 // Do not edit below this line
